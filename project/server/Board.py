cd8cba7025c37dd74c26cd56731b90da74414846
Emilia <emilia.a.wis@gmail.com>
2023-05-30 16:12:07 +0200

    dodanie pola startowego

--- project/server/Board.py
+++ project/server/Board.py
@@ -27,11 +27,16 @@ class Board:
             i += 1
         return None
 
+    def if_finished(self):
+        if len(self.fields[self.FIELDS - 1].get_state()) > 0:
+            return True
+        return False
+
     def move_turtle(self, turtle, val):
         place = self.find_turtle(turtle)
         if place is None:
             raise "turtle does not exist"
-        if self.FIELDS >= place + val >= 0:
+        if self.FIELDS > place + val >= 0:
             self.place_turtle(turtle, place + val)
         elif place + val < 0:
             self.place_turtle(turtle, 0)
@@ -47,6 +52,12 @@ if __name__ == "__main__":
 
     print(board.find_turtle("YELLOW"))
     print(board.fields[0].turtle_stack)
+
+    print(board.if_finished())
+
+    board.move_turtle("YELLOW", 10)
+    print(board.if_finished())
+
 
 
 
