1746c99d46134093b59a009aaaeaa2ac9288be1b
Roza <roza.wojcicka@gmail.com>
2023-06-01 20:52:32 +0200

    gra

--- project/server/game.py
+++ project/server/game.py
@@ -1,12 +1,38 @@
 from random import shuffle
-
+from player import Player
+from deck import Deck
+from card import Card
+from board import Board
+from turtle import Turtle
 
 class Game:
-    def __init__(self, id_players):
+    def __init__(self, ip_players): # gra dostaje liste adresow ip graczy
         self.players = []
-        turtles = ["RED","GREEN","BLUE","PURPLE","YELLOW"]
+        self.deck = Deck()
+        self.board = Board()
+        turtles = [Turtle("RED"),Turtle("GREEN"),Turtle("BLUE"),Turtle("PURPLE"),Turtle("YELLOW")]
         shuffle(turtles)
-        for i in range(len(id_players)):
-            self.players.append(Player(turtles[0],id_players[i]))
+        # tworze graczy przypisuje im losowe zolwie i ich adresy ip
+        for i in range(len(ip_players)):
+            self.players.append(Player(turtles[0],ip_players[i]))
             turtles.pop(0)
-    
+        # gracze dostaja po 5 kart na poczatku
+        for i in range(5):
+            for p in self.players:
+                p.add_card(self.deck.take_card)
+        # to teraz gramy dopoki nie jest koniec
+        i = 0
+        while self.board.finish != True:  # tu cos inaczej!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            if i >= len(self.players):
+                i = 0
+            turn(self.players[i])
+            i += 1
+        # co robimy na koncu gry?
+
+    def turn(self, player):
+        card = # gracz wybiera karte, ale jak?????????????????????????????????????????????????
+        self.board.accept_card(card) # karta rusza zlowiem
+        player.remove_card(card) # zabieramy ta karte z reki
+        self.deck.throw_card(card # wrzucamy na stos kart odrzuconych
+        player.add_card(self.deck.take_card) # dobiera nowa karte
+
